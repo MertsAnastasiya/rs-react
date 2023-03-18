20bcd8c9c0e226d55269553226c57bce021835b0
Anastasiya <nastya.mikholap@gmail.com>
2023-03-18 21:06:04 +0100

    feat: update component Card and create types for it

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -1,15 +1,20 @@
 import { Component } from 'react';
+import { CardProps } from 'types';
 import './Card.scss';
 
-export class Card extends Component {
+export class Card extends Component<CardProps, {}> {
   public render(): JSX.Element {
     return (
       <div className="card">
-        <img className="card__image"></img>
-        <p className="card__title">Title</p>
-        <p className="card__price">$536</p>
+        <img className="card__image" src={this.props.thumbnail}></img>
+        <p className="card__street">{this.props.street}</p>
+        <p className="card__city">{this.props.city}</p>
+        <p className="card__price">$ {this.props.price}</p>
+        <p className="card__detail">
+          <p className="card__square">{this.props.living} m2</p>
+          <p className="card__rooms">{this.props.rooms} rooms</p>
+        </p>
         <p className="card__like"></p>
-        <p className="card__like liked"></p>
       </div>
     );
   }
--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,35 @@
 export type InputProps = {
-    id: string,
-    classes: string,
-    placeholder: string,
-}
+  id: string;
+  classes: string;
+  placeholder: string;
+  searchValue: string;
+  changeStateBySeacrh: (searchValue: string) => void;
+  // searchData: (searchValue: string) => JSX.Element;
+  // searchData: (searchValue: string) => IProduct[];
+};
 
 export type InputState = {
-    searchValue: string,
+  searchValue: string;
+};
+
+export interface IProduct {
+  id: number;
+  street: string;
+  city: string;
+  price: number;
+  living: number;
+  rooms: number;
+  thumbnail: string;
+  // images: string[];
+}
+
+export type ProductProperty = keyof IProduct;
+
+export type CardProps = {
+  street: string;
+  city: string;
+  living: number;
+  rooms: number;
+  price: number;
+  thumbnail: string;
 }
