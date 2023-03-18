924cbc9a4e5b0e855b3c546067a17dcc4b7b17c2
Anastasiya <nastya.mikholap@gmail.com>
2023-03-18 11:13:42 +0100

    feat: save search value to LocalStorage

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -5,31 +5,37 @@ import './Input.scss';
 export class Seacrh extends Component<InputProps, InputState> {
   constructor(props: InputProps) {
     super(props);
+
     this.state = {
-      searchValue: '',
+      searchValue: localStorage.getItem('searchValue') || '',
     };
   }
 
   private handleKeyUp = (event: React.KeyboardEvent<HTMLInputElement>): void => {
     if (event.key === 'Enter') {
-      const target: HTMLInputElement = event.target as HTMLInputElement;
-      this.setState({
-        searchValue: target.value,
-      });
       localStorage.setItem('searchValue', this.state.searchValue);
-      console.log('You pushed Enter');
     }
   };
 
+  private handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const target: HTMLInputElement = event.target as HTMLInputElement;
+    this.setState({
+      searchValue: target.value,
+    });
+  }
+
   public render(): JSX.Element {
     const { id, classes, placeholder } = this.props;
+    const { searchValue } = this.state;
     return (
       <input
         type="text"
+        value={searchValue}
         name={id}
         className={classes}
         placeholder={placeholder}
         onKeyUp={this.handleKeyUp}
+        onChange={this.handleChange}
       />
     );
   }
