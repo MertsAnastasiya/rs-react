7d5c28c2a26a593821f7795cb29230c44aab376f
Anastasiya <nastya.mikholap@gmail.com>
2023-03-18 21:06:27 +0100

    feat: update component Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,18 +2,54 @@ import { Component, Fragment } from 'react';
 import { Seacrh } from './input/Input';
 import { createClassList } from '../utils';
 import { Card } from './cards/Card';
+import { productsData } from '../data';
+import { IProduct, ProductProperty } from 'types';
+import { CardsList } from './cards/CardsList';
 
-export class Home extends Component<{}, {}> {
-  render(): JSX.Element {
+export class Home extends Component<{}, {searchValue: string}> {
+  public state = {
+    searchValue: localStorage.getItem('searchValue') || '',
+  }
+
+  private searchData = (searchValue: string): IProduct[] => {
+    let productsResult: IProduct[] = [];
+    productsData.forEach((product) => {
+      let addToResult: boolean = false;
+      const propertiesOfProduct: ProductProperty[] = Object.keys(product) as ProductProperty[];
+
+      propertiesOfProduct.forEach((property) => {
+        if (
+            product[property]
+                .toString()
+                .toLowerCase()
+                .includes(searchValue.toLowerCase())
+        ) {
+            addToResult = true;
+        }
+      });
+
+      if (addToResult) productsResult.push(product);
+    });
+
+    return productsResult;
+  }
+
+  private changeStateBySeacrh = (searchInputValue: string): void => {
+    this.setState({searchValue: searchInputValue});
+  }
+
+  public render(): JSX.Element {
     return (
       <Fragment>
         <h1 className="h1">Home</h1>
         <Seacrh
           id="search"
           classes={createClassList(['input', 'input_search'])}
-          placeholder="Search"
+          placeholder="What are you looking for?"
+          searchValue={this.state.searchValue}
+          changeStateBySeacrh={this.changeStateBySeacrh}
         />
-        <Card />
+        <CardsList productResult={this.searchData(this.state.searchValue)} />
       </Fragment>
     );
   }
