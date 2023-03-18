403ed32ba7be2a1aa845097fc76f407f105d6647
Anastasiya <nastya.mikholap@gmail.com>
2023-03-18 21:05:17 +0100

    feat: create component CardList

---
+++ src/components/cards/CardsList.tsx
@@ -0,0 +1,28 @@
+import { IProduct } from 'types';
+import { Card } from './Card';
+import './Card.scss';
+
+type CardsListProps = {
+  productResult: IProduct[];
+};
+
+export const CardsList = (props: CardsListProps): JSX.Element => {
+  return (
+    <div className="card-list">
+      {props.productResult.map((item) => {
+        const { id, street, city, price, thumbnail, living, rooms } = item;
+        return (
+          <Card
+            key={id}
+            street={street}
+            price={price}
+            city={city}
+            thumbnail={thumbnail}
+            living={living}
+            rooms={rooms}
+          />
+        );
+      })}
+    </div>
+  );
+};
