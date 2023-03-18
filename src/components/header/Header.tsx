3345e245ead02ff8a7e2d8086798b20afd370f30
Anastasiya <nastya.mikholap@gmail.com>
2023-03-18 22:16:46 +0100

    feat: create component Header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,21 @@
+import { NavLink } from 'react-router-dom';
+import './Header.scss';
+
+export const Header = (): JSX.Element => {
+  return (
+    <header className="header">
+      <div className="container header__container">
+        <nav>
+          <ul className="navigation__list">
+            <li>
+              <NavLink to={'/'} className="link">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to={'/about'} className="link">About Us</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
