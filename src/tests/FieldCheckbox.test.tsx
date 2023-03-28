8e1adac7f73cfd559beabc1475741da3973555de
Anastasiya <nastya.mikholap@gmail.com>
2023-03-28 20:15:55 +0200

    feat: add test for FieldCheckbox

--- src/tests/FieldCheckbox.test.tsx
+++ src/tests/FieldCheckbox.test.tsx
@@ -5,7 +5,7 @@
 import * as ReactDOMClient from 'react-dom/client';
 import { act } from '@testing-library/react';
 import React, { createRef } from 'react';
-import { FieldInput } from '../components/form/FieldInput';
+import { FieldCheckbox } from '../components/form/FieldCheckbox';
 
 let container: Element | null = null;
 
@@ -14,29 +14,27 @@ beforeEach(() => {
   document.body.appendChild(container);
 });
 
-describe('FieldInput component', () => {
-  it('renders FieldInput component', () => {
+describe('FieldCheckbox component', () => {
+  it('renders FieldCheckbox component', () => {
     act(() => {
       if (!container) {
         return;
       }
       const root = ReactDOMClient.createRoot(container);
       const handleChangeInput = () => console.log('Input was changing');
-      const inputStreetRef: React.RefObject<HTMLInputElement> = createRef<HTMLInputElement>();
+      const checkboxBalconyRef: React.RefObject<HTMLInputElement> = createRef<HTMLInputElement>();
       root.render(
-        <FieldInput
-            type="text"
-            id="street"
-            ref={inputStreetRef}
-            label="Street:"
-            classNames={['input', 'input_text']}
-            onChange={handleChangeInput}
-      />
+        <FieldCheckbox
+                  ref={checkboxBalconyRef}
+                  classNames={['label']}
+                  label="garage"
+                  onChange={handleChangeInput}
+                />
       );
     });
     if (!container) {
       return;
     }
-    expect(container.textContent).toContain('Street:');
+    expect(container.textContent).toContain('garage');
   });
 });
